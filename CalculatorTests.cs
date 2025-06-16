b8e9dd9e049fe40c9e9c0462a5c9654c6d1554a7
Ajay-coder-hash <at753820@gmail.com>
2025-06-16 12:14:57 -0400

    Add files via upload
    these are for unit testing.

---
+++ CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using SportFwd.Service;
+using Xunit;
+
+namespace SportFwd.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            int result = calc.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
